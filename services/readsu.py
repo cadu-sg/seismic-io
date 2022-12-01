65eae1b1c2448a8265d2c8054d5a4c3139e1c642
JorbFreire <jorbfreire@gmail.com>
2022-12-01 17:58:24 -0300

    remove print

--- services/readsu.py
+++ services/readsu.py
@@ -2,6 +2,5 @@ from ..Models.InOutSuModel import InOutSu
 
 def readsu(file_path):
 	# Read a binary file in .su format
-	print("readsu")
 	with open(file_path, 'rb') as file:
 		return InOutSu.unpack_su(file)
