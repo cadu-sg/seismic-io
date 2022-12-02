7eec41bb05caf40424223d34654be6d75b9b3a2c
JorbFreire <jorbfreire@gmail.com>
2022-12-02 01:13:14 -0300

    add __version__ to read on running python

--- seismicio/__init__.py
+++ seismicio/__init__.py
@@ -2,3 +2,4 @@ from .services.readsu import readsu
 from .services.readsuInMemory import readsuInMemory
 from .services.writesu import writesu
 from .services.writesuInMemory import writesuInMemory
+from .constants.__version__ import __version__
